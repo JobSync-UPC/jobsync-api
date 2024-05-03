d31dfbc8762077fa284a2ffcad0ae69259a6d9b5
Carlos Chávez <u201910317@upc.edu.pe>
2024-05-03 04:01:50 -0500

    Update RecruitmentProcessServiceImplTest.java

--- src/test/java/com/jobsync/jobysncapi/organization/service/impl/RecruitmentProcessServiceImplTest.java
+++ src/test/java/com/jobsync/jobysncapi/organization/service/impl/RecruitmentProcessServiceImplTest.java
@@ -1,24 +1,85 @@
 package com.jobsync.jobysncapi.organization.service.impl;
 
+import com.jobsync.jobysncapi.organization.domain.model.entity.RecruitmentProcess;
+import com.jobsync.jobysncapi.organization.domain.persistence.CompanyRepository;
+import com.jobsync.jobysncapi.organization.domain.model.entity.Company;
+import com.jobsync.jobysncapi.organization.domain.persistence.RecruitmentProcessRepository;
+import com.jobsync.jobysncapi.organization.service.RecruitmentProcessService;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class RecruitmentProcessServiceImplTest {
 
+    @Mock
+    private RecruitmentProcessRepository recruitmentProcessRepository;
+
+    @Mock
+    private CompanyRepository companyRepository;
+
+    @InjectMocks
+    private RecruitmentProcessServiceImpl recruitmentProcessService;
+
     @Test
-    void createRecruitmentProcess() {
+    public void testCreateRecruitmentProcess_Success() {
+        Long companyId = 1L;
+        Company expectedCompany = new Company();
+
+        Mockito.when(companyRepository.findById(companyId)).thenReturn(Optional.of(expectedCompany));
+        Mockito.when(recruitmentProcessRepository.save(Mockito.any(RecruitmentProcess.class))).thenReturn(new RecruitmentProcess());
+
+        RecruitmentProcess createdProcess = recruitmentProcessService.createRecruitmentProcess(companyId);
+
+        assertNotNull(createdProcess);
     }
 
     @Test
-    void updateRecruitmentProcess() {
+    public void testUpdateRecruitmentProcess() {
+        Long recruitmentProcessId = 1L;
+        RecruitmentProcess existingProcess = new RecruitmentProcess();
+        existingProcess.setId(recruitmentProcessId);
+        existingProcess.setEnabled(false);
+
+        Mockito.when(recruitmentProcessRepository.findById(recruitmentProcessId)).thenReturn(Optional.of(existingProcess));
+        Mockito.when(recruitmentProcessRepository.save(existingProcess)).thenReturn(existingProcess);
+
+        RecruitmentProcess updatedProcess = recruitmentProcessService.updateRecruitmentProcess(recruitmentProcessId);
+
+        assertTrue(updatedProcess.getEnabled());
     }
 
     @Test
-    void getAllRecruitmentProcesses() {
+    public void testDeleteRecruitmentProcess() {
+        Long recruitmentProcessId = 1L;
+        RecruitmentProcess existingProcess = new RecruitmentProcess();
+        existingProcess.setId(recruitmentProcessId);
+
+        Mockito.when(recruitmentProcessRepository.findById(recruitmentProcessId)).thenReturn(Optional.of(existingProcess));
+        Mockito.when(recruitmentProcessRepository.save(existingProcess)).thenReturn(existingProcess);
+
+        recruitmentProcessService.deleteRecruitmentProcess(recruitmentProcessId);
+
+        //no se como hacer para que verifique que se ha borrado correctamente
     }
 
     @Test
-    void getRecruitmentProcessById() {
+    public void testGetRecruitmentProcessById() {
+        Long recruitmentProcessId = 1L;
+        RecruitmentProcess expectedProcess = new RecruitmentProcess();
+
+        Mockito.when(recruitmentProcessRepository.findById(recruitmentProcessId)).thenReturn(Optional.of(expectedProcess));
+
+        RecruitmentProcess actualProcess = recruitmentProcessService.getRecruitmentProcessById(recruitmentProcessId);
+
+        assertEquals(actualProcess,expectedProcess);
     }
 }
