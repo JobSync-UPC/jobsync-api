ad539cca9ca0315fbbfc42aef61a3983bbfcb453
GabbrM <115942603+GabbrM@users.noreply.github.com>
2024-05-18 10:45:26 -0500

    feat: add recruitment phase

--- src/main/java/com/jobsync/jobysncapi/organization/service/RecruitmentPhaseService.java
+++ src/main/java/com/jobsync/jobysncapi/organization/service/RecruitmentPhaseService.java
@@ -1,6 +1,22 @@
 package com.jobsync.jobysncapi.organization.service;
 
+import com.jobsync.jobysncapi.organization.api.dto.request.RecruitmentPhaseRequest;
+import com.jobsync.jobysncapi.organization.api.dto.response.RecruitmentPhaseResponse;
+import com.jobsync.jobysncapi.organization.domain.model.entity.RecruitmentPhase;
+
+import java.util.Optional;
+
 public interface RecruitmentPhaseService {
+
+
+    public abstract Iterable<RecruitmentPhaseResponse> getAllRecruitmentPhases();
+    public abstract Optional<RecruitmentPhaseResponse> getRecruitmentPhaseById(Long recruitmentPhaseId);
+
+    public abstract RecruitmentPhase createRecruitmentPhase(RecruitmentPhaseRequest recruitmentPhaseRequest);
+
+    public abstract RecruitmentPhase updateRecruitmentPhase(Long recruitmentPhaseId, RecruitmentPhaseRequest recruitmentPhaseRequest);
+
+    public abstract void deleteRecruitmentPhase(Long recruitmentPhaseId);
 
 
 }
