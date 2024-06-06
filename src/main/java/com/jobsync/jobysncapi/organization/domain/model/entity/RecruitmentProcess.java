561590f2dbf0b767827b6591e66c98ba56200a20
sayufu <lilianafuye@gmail.com>
2024-06-06 01:34:03 -0500

    fix: fix recruitment

--- src/main/java/com/jobsync/jobysncapi/organization/domain/model/entity/RecruitmentProcess.java
+++ src/main/java/com/jobsync/jobysncapi/organization/domain/model/entity/RecruitmentProcess.java
@@ -1,5 +1,6 @@
 package com.jobsync.jobysncapi.organization.domain.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,6 +33,7 @@ public class RecruitmentProcess {
     @JoinColumn(name = "job_post_id", referencedColumnName = "id")
     private JobPost jobPost;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     private Date created_date;
 
     private Boolean enabled = true;
